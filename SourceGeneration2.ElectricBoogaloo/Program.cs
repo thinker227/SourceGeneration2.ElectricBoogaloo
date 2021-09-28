ea3d372218686a79f8e673c7ab0c052402fdddca
thinker227 <thinker0227@gmail.com>
2021-09-28 19:05:40 +0200

    Initial commit

---
+++ SourceGeneration2.ElectricBoogaloo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SourceGeneration2.ElectricBoogaloo {
+	internal static partial class Program {
+
+		private static void Main() {
+			HelloFrom("Generated code");
+		}
+
+		static partial void HelloFrom(string name);
+
+	}
+}
---
+++ SourceGeneration2.Generator/MainGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace SourceGeneration2.Generator {
+	[Generator]
+	public class MainGenerator : ISourceGenerator {
+
+		public void Execute(GeneratorExecutionContext context) {
+			var main = context.Compilation.GetEntryPoint(context.CancellationToken);
+
+			string source = $@"
+using System;
+
+namespace {main.ContainingNamespace.Name} {{
+	partial class {main.ContainingType.Name} {{
+		
+		private static partial void HelloFrom(string name) =>
+			Console.WriteLine($""Hi from '{{name}}'"");
+		
+	}}
+}}
+";
+
+			context.AddSource("GeneratedSource", source);
+		}
+
+		public void Initialize(GeneratorInitializationContext context) {
+
+		}
+
+	}
+}
